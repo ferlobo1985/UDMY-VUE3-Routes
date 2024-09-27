7f67ef1982587d7da7d9288459e7aadd0bc2ff29
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-27 14:55:49 -0300

    10-Global route guard
    10-Global route guard

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '@/components/home.vue'
 import Article from '@/components/articles/article.vue';
 import NotFound from '@/components/404.vue';
 import Notify from '@/components/notify.vue';
+import Login from '@/components/login.vue';
 
 const propsBack = (route) => {
     /////
@@ -24,9 +25,28 @@ const router = createRouter({
             default:Contact,
             notify:Notify
         }, name:'contact'},
+        {path:'/login',component:Login},
         {path:'/:noFound(.*)',component:NotFound}
     ],
     linkActiveClass:'active'
 });
+
+
+router.beforeEach((to,from,next)=>{
+    const isAuth = true;
+
+    if(to.path === '/'){
+        next();
+    } else {
+        if(to.path !== '/login' && !isAuth) return next({path:'/login'});
+        if(to.path === '/login' && isAuth) return next({path:'/'})
+        return next();
+    }
+})
+
+router.afterEach(()=>{
+    console.log('after');
+})
+
 
 export default router;
